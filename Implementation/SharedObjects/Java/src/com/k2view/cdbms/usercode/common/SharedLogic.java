6923d0e36598eb1bd9b2c2cfcda28457c79f1838
zelalem gelgelo <zelalem.gelgelo@k2view.com>
2020-10-14 23:25:19 -0500

    First commit

---
+++ Implementation/SharedObjects/Java/src/com/k2view/cdbms/usercode/common/SharedLogic.java
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////////////////////////////////
+// Project Shared Functions
+/////////////////////////////////////////////////////////////////////////
+
+package com.k2view.cdbms.usercode.common;
+
+import java.util.*;
+import java.sql.*;
+import java.math.*;
+import java.io.*;
+
+import com.k2view.cdbms.shared.*;
+import com.k2view.cdbms.shared.user.UserCode;
+import com.k2view.cdbms.sync.*;
+import com.k2view.cdbms.lut.*;
+import com.k2view.cdbms.shared.logging.LogEntry.*;
+import com.k2view.cdbms.func.oracle.OracleToDate;
+import com.k2view.cdbms.func.oracle.OracleRownum;
+import com.k2view.cdbms.shared.utils.UserCodeDescribe.*;
+
+import static com.k2view.cdbms.shared.user.ProductFunctions.*;
+import static com.k2view.cdbms.shared.user.UserCode.*;
+import static com.k2view.cdbms.shared.utils.UserCodeDescribe.FunctionType.*;
+import static com.k2view.cdbms.usercode.common.SharedGlobals.*;
+
+@SuppressWarnings({"unused", "DefaultAnnotationParam"})
+public class SharedLogic {
+
+	
+	
+	
+
+}
